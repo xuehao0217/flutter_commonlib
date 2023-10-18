34a4173992afc8d0f2dc6e1e4762e8a11597e2fb
xuehao <xuehao@deepway.ai>
2023-10-18 16:58:19 +0800

    添加生成水印

--- lib/helpter/image_loader_utils.dart
+++ lib/helpter/image_loader_utils.dart
@@ -9,82 +9,62 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
 
-/// 图片加载工具类
 class ImageLoaderUtils {
-  //私有化构造
-  ImageLoaderUtils._();
+  static Future<Uint8List?> createImageFromWidget(
+      BuildContext context, Widget widget,
+      {Duration? wait, Size? logicalSize, Size? imageSize}) async {
+    final repaintBoundary = RenderRepaintBoundary();
 
-  //单例模式创建
-  static final ImageLoaderUtils imageLoader = ImageLoaderUtils._();
+    logicalSize ??=
+        View.of(context).physicalSize / View.of(context).devicePixelRatio;
+    imageSize ??= View.of(context).physicalSize;
 
-  // 将一个Widget转为image.Image对象
-  Future<ui.Image> getImageFromWidget(GlobalKey globalKey) async {
-    // globalKey为需要图像化的widget的key
-    RenderRepaintBoundary? boundary =
-    globalKey.currentContext?.findRenderObject() as RenderRepaintBoundary?;
-    // 转换为图像
-    ui.Image img = await boundary!.toImage(pixelRatio: 6);
-    return img;
-  }
+    assert(logicalSize.aspectRatio == imageSize.aspectRatio,
+    'logicalSize and imageSize must not be the same');
 
-  ///将指定的文件保存到目录空间中。
-  ///[image] 这里是使用的ui包下的Image
-  ///[picName] 保存到本地的文件（图片）文件名，如test_image
-  ///[endFormat]保存到本地的文件（图片）文件格式，如png，
-  ///[isReplace]当本地存在同名的文件（图片）时，true就是替换
-  ///[isEncode]对保存的文件（图片）进行编码
-  ///  最终保存到本地的文件 （图片）的名称为 picName.endFormat
-  static Future<String> saveImageByUIImage(
-      ui.Image image,
-      {String? picName,
-        String endFormat = "png",
-        bool isReplace = true,
-        bool isEncode = true}) async {
-    ///获取本地磁盘路径
-    /*
-     * 在Android平台中获取的是/data/user/0/com.studyyoun.flutterbookcode/app_flutter
-     * 此方法在在iOS平台获取的是Documents路径
-     */
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    String appDocPath = appDocDir.path;
+    final renderView = RenderView(
+        child: RenderPositionedBox(
+            alignment: Alignment.center, child: repaintBoundary),
+        configuration: ViewConfiguration(
+          size: logicalSize,
+          devicePixelRatio: 1,
+        ),
+        view: View.of(context) //PlatformDispatcher.instance.views.first,
+    );
 
-    ///拼接目录
-    if (picName == null || picName.trim().length == 0) {
-      ///当用户没有指定picName时，取当前的时间命名
-      picName = "${DateTime.now().millisecond.toString()}.$endFormat";
-    } else {
-      picName = "$picName.$endFormat";
+    final pipelineOwner = PipelineOwner();
+    final buildOwner = BuildOwner(focusManager: FocusManager());
+
+    pipelineOwner.rootNode = renderView;
+    renderView.prepareInitialFrame();
+
+    final rootElement = RenderObjectToWidgetAdapter<RenderBox>(
+        container: repaintBoundary,
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: widget,
+        )).attachToRenderTree(buildOwner);
+
+    buildOwner.buildScope(rootElement);
+
+    if (wait != null) {
+      await Future.delayed(wait);
     }
 
-    if (isEncode) {
-      ///对保存的图片名字加密
-      picName = picName.toString();
-    }
+    buildOwner
+      ..buildScope(rootElement)
+      ..finalizeTree();
 
-    appDocPath = "$appDocPath/$picName";
+    pipelineOwner
+      ..flushLayout()
+      ..flushCompositingBits()
+      ..flushPaint();
 
-    ///校验图片是否存在
-    var file = File(appDocPath);
-    bool exist = await file.exists();
-    if (exist) {
-      if (isReplace) {
-        ///如果图片存在就进行删除替换
-        ///如果新的图片加载失败，那么旧的图片也被删除了
-        await file.delete();
-      } else {
-        ///如果图片存在就不进行下载
-        return "";
-      }
-    }
-    ByteData? byteData = await image.toByteData(format: ImageByteFormat.png);
-    Uint8List pngBytes = byteData!.buffer.asUint8List();
-    print("保存的图片路径 $appDocPath");
+    final image = await repaintBoundary.toImage(
+        pixelRatio: imageSize.width / logicalSize.width);
+    final byteData = await image.toByteData(format: ImageByteFormat.png);
 
-    ///将Uint8List的数据格式保存
-    await File(appDocPath).writeAsBytes(pngBytes);
-
-    return appDocPath;
+    return byteData?.buffer.asUint8List();
   }
 }
--- lib/ui/camera_watermark.dart
+++ lib/ui/camera_watermark.dart
@@ -15,6 +15,7 @@ import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import '../base/mvvm/base_stateful_widget.dart';
+import '../helpter/image_loader_utils.dart';
 import '../widget/common_widget.dart';
 
 class WatermarkPage extends StatefulWidget {
@@ -86,6 +87,21 @@ class _WatermarkPage extends BaseStatefulWidget<WatermarkViewModel> {
             style: TextStyle(color: Colors.white, fontSize: 16),
           ),
         ).intoPadding(const EdgeInsets.only(bottom: 15, left: 15, right: 15)),
+
+        CommonButton(
+          elevation: 2,
+          circular: 10,
+          backgroundColor: Colors.blue,
+          width: double.infinity,
+          height: 50,
+          onPressed: ()  {
+            getImageToImage();
+          },
+          child: const Text(
+            "不显示照片打水印",
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ).intoPadding(const EdgeInsets.only(bottom: 15, left: 15, right: 15)),
       ],
     );
   }
@@ -134,5 +150,64 @@ class _WatermarkPage extends BaseStatefulWidget<WatermarkViewModel> {
     image
         .toByteData(format: ImageByteFormat.png)
         .then((value) => {_saveImage(value!.buffer.asUint8List())});
+  }
+
+  Future getImageToImage() async {
+    final pickedFile = await picker.pickImage(
+      // 拍照获取图片
+      source: ImageSource.camera,
+      // 手机选择图库
+      //   source: ImageSource.gallery,
+    );
+    setState(() {
+      if (pickedFile != null) {
+        captureWidgetToImage(context, pickedFile.path);
+      } else {
+        SmartDialog.showToast('没有选择任何图片');
+      }
+    });
+  }
+
+  //widget不能在页面展示，需要在后台构建指定的UI，获取widget的截图并生成图片保存起来。
+  void captureWidgetToImage(BuildContext context, String path) async {
+    final widgetToCapture = Stack(
+      alignment: Alignment.center, // 子部件在堆叠中的对齐方式
+      children: <Widget>[
+        Container(
+          width: double.infinity,
+          child: Image.file(File(path)),
+        ),
+        Positioned(
+          bottom: 20,
+          right: 20,
+          child: Container(
+            width: 50,
+            color: Colors.green,
+            child: const Text(
+              'Stack Example',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    final Uint8List? imageUint8List =
+        await ImageLoaderUtils.createImageFromWidget(context, widgetToCapture,
+            wait: const Duration(milliseconds: 1000),
+            imageSize: const Size(200, 200),
+            logicalSize: const Size(220, 220));
+
+    if (imageUint8List != null) {
+      setState(() {
+        _saveImage(imageUint8List);
+      });
+      print('Image captured successfully.');
+    } else {
+      print('Image capture failed.');
+    }
   }
 }
