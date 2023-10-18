5d8ce84d507622b33e6d55b7e8f5e8ff135fe750
xuehao <xuehao@deepway.ai>
2023-10-18 15:16:32 +0800

    添加输入框

--- lib/ui/camera_watermark.dart
+++ lib/ui/camera_watermark.dart
@@ -14,12 +14,9 @@ import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
-
-import '../base/base_page_stateful_widget.dart';
 import '../base/mvvm/base_stateful_widget.dart';
-import '../helpter/image_loader_utils.dart';
 import '../widget/common_widget.dart';
-import 'package:image/image.dart' as img;
+
 class WatermarkPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _WatermarkPage();
@@ -29,30 +26,32 @@ class _WatermarkPage extends BaseStatefulWidget<WatermarkViewModel> {
   // 实例化
   final picker = ImagePicker();
   var _image = "".obs;
-  final GlobalKey _widgetKey = GlobalKey();
+  final GlobalKey globalKey = GlobalKey();
 
   @override
   Widget buildPageContent(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
-
-         ConstraintLayout(
-            key: _widgetKey,
-            children: [
-              Obx(() => Image.file(File(_image.value)).applyConstraint(
-                id: cId("img"),
-                top: parent.top,
-                width: matchParent,
-              )),
-              const Text(
-                "我是水印",
-                style: TextStyle(color: Colors.red, fontSize: 15),
-              ).applyConstraint(
-                right: cId("img").right,
-                bottom: cId("img").bottom,
-              )
-            ],
-        ),
+        if (_image.value.isNotEmpty)
+          RepaintBoundary(
+            key: globalKey,
+            child: ConstraintLayout(
+              children: [
+                Obx(() => Image.file(File(_image.value)).applyConstraint(
+                      id: cId("img"),
+                      top: parent.top,
+                      width: matchParent,
+                    )),
+                const Text(
+                  "我是水印",
+                  style: TextStyle(color: Colors.red, fontSize: 15),
+                ).applyConstraint(
+                  right: cId("img").right,
+                  bottom: cId("img").bottom,
+                )
+              ],
+            ),
+          ),
         CommonButton(
           elevation: 2,
           circular: 10,
@@ -63,25 +62,30 @@ class _WatermarkPage extends BaseStatefulWidget<WatermarkViewModel> {
             getImage();
           },
           child: const Text(
-            "拍照打水印",
+            "拍照",
             style: TextStyle(color: Colors.white, fontSize: 16),
           ),
         ).intoPadding(const EdgeInsets.only(bottom: 15, left: 15, right: 15)),
+        if (saveImagePath.value.isNotEmpty)
+          Image.file(File(saveImagePath.value)),
+        // Image.memory(
+        //   generatedImage.value!!,
+        // ),
+
         CommonButton(
           elevation: 2,
           circular: 10,
           backgroundColor: Colors.blue,
           width: double.infinity,
           height: 50,
-          onPressed: () async {
-            _generatePicWidget();
+          onPressed: () {
+            generateImage();
           },
           child: const Text(
-            "拍照打水印1111",
+            "打水印",
             style: TextStyle(color: Colors.white, fontSize: 16),
           ),
         ).intoPadding(const EdgeInsets.only(bottom: 15, left: 15, right: 15)),
-
       ],
     );
   }
@@ -94,10 +98,9 @@ class _WatermarkPage extends BaseStatefulWidget<WatermarkViewModel> {
       //   source: ImageSource.gallery,
     );
     // 更新状态
-    setState(() async {
+    setState(() {
       if (pickedFile != null) {
         _image.value = pickedFile.path;
-        _generatePicWidget();
       } else {
         SmartDialog.showToast('没有选择任何图片');
       }
@@ -105,35 +108,33 @@ class _WatermarkPage extends BaseStatefulWidget<WatermarkViewModel> {
   }
 
   @override
-  WatermarkViewModel createViewModel() =>WatermarkViewModel();
+  WatermarkViewModel createViewModel() => WatermarkViewModel();
 
   @override
-  void initData() {
+  void initData() {}
+
+  var saveImagePath = "".obs;
+
+  Future _saveImage(Uint8List generatedImage) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/my_image.png');
+    var file2 = await file.writeAsBytes(generatedImage);
+    setState(() {
+      showToast(file2.path);
+      saveImagePath.value = file2.path;
+    });
   }
 
-
-  /// 通过 RenderRepaintBoundary 生成图片
-  void _generatePicWidget() async {
-    //根据Globalkey获取RenderObject对象
-    final boundary = _widgetKey.currentContext?.findRenderObject();
-    if (boundary != null && boundary is RenderRepaintBoundary) {
-      final image = await boundary.toImage();
-      ByteData? byteData = await image.toByteData(format: ImageByteFormat.png);
-      if (byteData != null) {
-        //获取当前文件目录
-        Directory fileDirectory = await getApplicationDocumentsDirectory();
-        String filePath = fileDirectory.path;
-        Directory directory;
-        directory = await Directory(
-            '$filePath${Platform.pathSeparator}picuser')
-            .create(recursive: true);
-        assert(await directory.exists() == true);
-        //将字节流数据转Unit8编码
-        Uint8List imageData = byteData.buffer.asUint8List();
-        //将数据写入目录下的文件
-        File("${directory.absolute.path}/temp.png").writeAsBytes(imageData);
-        SmartDialog.showToast(filePath+"--------");
-      }
-    }
+  // var isImageGenerated = Rx<bool>(false);
+  // var generatedImage = Rx<Uint8List?>(null);
+  Future generateImage() async {
+    final boundary =
+        globalKey.currentContext?.findRenderObject() as RenderRepaintBoundary;
+    final image = await boundary.toImage(pixelRatio: 3); // 调整分辨率
+    final byteData = await image.toByteData(format: ImageByteFormat.png);
+    final uint8list = byteData!.buffer.asUint8List();
+    setState(() {
+      _saveImage(uint8list);
+    });
   }
 }
