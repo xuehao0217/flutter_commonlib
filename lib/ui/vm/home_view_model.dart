eb9ea648b6c0d263f0e41113ca800a7e12bcfbef
xuehao <xuehao@deepway.ai>
2023-10-24 10:43:50 +0800

    下载apk

--- lib/ui/vm/home_view_model.dart
+++ lib/ui/vm/home_view_model.dart
@@ -6,6 +6,8 @@ import 'package:flutter_commonlib/entity/home_banner_entity.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../api/http_api.dart';
 import '../../base/mvvm/base_view_model.dart';
@@ -25,9 +27,9 @@ class HomeViewModel extends BaseViewModel {
     });
   }
 
-
   var page = 0;
   var homeDatas = <HomeListDatas>[].obs;
+
   void getHomeData(VoidCallback? onfinally, {bool refresh = false}) {
     if (refresh) {
       page = 0;
@@ -43,5 +45,33 @@ class HomeViewModel extends BaseViewModel {
         homeDatas.value.addAll(data!.datas);
       }
     }, onfinally: onfinally);
+  }
+
+
+  void downApkFunction(String apkUrl) async {
+    final status = await Permission.storage.request();
+    if (status != PermissionStatus.granted) {
+      print('storage permission not granted');
+    } else {
+      String savePath = await getPhoneLocalPath();
+      String appName = "dw.apk";
+      await HttpUtils.instance.dio.download(apkUrl, "$savePath$appName",
+          onReceiveProgress: (received, total) {
+        if (total != -1) {
+          ///当前下载的百分比例
+          print("${(received / total * 100).toStringAsFixed(0)}%");
+          // CircularProgressIndicator(value: currentProgress,) 进度 0-1
+          var currentProgress = received / total;
+          if (received == total) {
+            print("下载完成 ${savePath}");
+          }
+        }
+      });
+    }
+  }
+
+  Future<String> getPhoneLocalPath() async {
+    final directory = await getApplicationCacheDirectory();
+    return directory!.path;
   }
 }
