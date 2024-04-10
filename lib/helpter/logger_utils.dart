8302b9e6fc08441ab8c8b0c3c8c0974910341eca
x <644738532@qq.com>
2024-04-10 16:23:29 +0800

    优化代码

--- lib/helpter/logger_utils.dart
+++ lib/helpter/logger_utils.dart
@@ -3,11 +3,17 @@ import 'package:logger/logger.dart';
 class LoggerUtils {
   static Logger logger = Logger();
 
-  static void d(String msg) {
-    logger.d(msg);
+  static void d(
+      String msg, {
+        Object? error,
+      }) {
+    logger.d(msg, error: error);
   }
 
-  static void e(String msg) {
-    logger.e(msg);
+  static void e(
+      String msg, {
+        Object? error,
+      }) {
+    logger.e(msg, error: error);
   }
 }
