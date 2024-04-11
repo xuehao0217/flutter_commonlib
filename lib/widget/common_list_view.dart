1d178ccd706ddeb286773a2d4ea834a8b85c82f2
x <644738532@qq.com>
2024-04-11 18:57:24 +0800

    优化刷新

--- lib/widget/common_list_view.dart
+++ lib/widget/common_list_view.dart
@@ -101,8 +101,8 @@ class CommonListWidget extends StatelessWidget {
         enableBallisticLoad: true,
         // 可以通过惯性滑动触发加载更多
         child: SmartRefresher(
-          enablePullUp: true,
-          enablePullDown: true,
+          enablePullUp: enableRefresh,
+          enablePullDown: enableLoad,
           header: const ClassicHeader(
             refreshingText: "加载中...",
             releaseText: "放开刷新",
