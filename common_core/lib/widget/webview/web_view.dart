1e48f62f2055e1b46a245afb281dcb85f04655f1
x <xuehao0217@gmail.com>
2025-10-23 19:16:30 +0800

    优化代码

--- common_core/lib/widget/webview/web_view.dart
+++ common_core/lib/widget/webview/web_view.dart
@@ -128,6 +128,11 @@ class _WebViewPageState extends BaseStatefulWidget<WebViewPage> {
     );
   }
 
+  @override
+  Widget? setRightTitleContent() {
+    // 使用单例配置，通过当前 WebView 的 url 获取对应的 Widget
+    return WebBuildConfig().buildRightTitleContent(context, widget.url);
+  }
 
   @override
   Future<void> onBackPressed() async {
@@ -140,4 +145,38 @@ class _WebViewPageState extends BaseStatefulWidget<WebViewPage> {
 }
 
 
+void main(){
+  // 配置 WebView 右侧标题内容
+  WebBuildConfig().setRightTitleBuilder((context, url) {
+    if (url.contains("baidu")) {
+     return Row();
+    }
+    return Row(); // 默认空
+  });
+}
+
+/// 单例 WebBuildConfig
+class WebBuildConfig {
+  WebBuildConfig._privateConstructor();
+
+  static final WebBuildConfig _instance = WebBuildConfig._privateConstructor();
+
+  factory WebBuildConfig() => _instance;
+
+  /// 回调类型：传入 context 和当前 url，返回 Widget
+  Widget Function(BuildContext context, String url)? rightTitleBuilder;
+
+  /// 设置右侧标题内容回调
+  void setRightTitleBuilder(Widget Function(BuildContext context, String url) builder) {
+    rightTitleBuilder = builder;
+  }
+
+  /// 获取右侧标题 Widget
+  Widget buildRightTitleContent(BuildContext context, String url) {
+    if (rightTitleBuilder != null) {
+      return rightTitleBuilder!(context, url);
+    }
+    return Row(); // 默认空
+  }
+}
 
