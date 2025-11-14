c13ebe5da8e6f986d5de2b6d55471c2309a9f023
x <xuehao0217@gmail.com>
2025-11-14 10:42:12 +0800

    加入android配置

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,6 +30,15 @@ android {
         versionName = flutter.versionName
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
@@ -41,4 +50,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // 解决firebase
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
--- common_core/lib/base/mvvm/base_list_view_model.dart
+++ common_core/lib/base/mvvm/base_list_view_model.dart
@@ -5,13 +5,10 @@ import 'base_view_model.dart';
 abstract class BaseListViewModel<T> extends BaseViewModel {
   final datas = <T>[].obs;
 
-  /// ：请求一页数据，返回 List<T> 和 total（可选）
   Future<List<T>> fetchPage(int page, int size);
 
-  /// 页码、总数、每页数量
   int _pageNumber = 1;
-  int _pageSize = 20;
-  int _totalCount = 0;
+  int _pageSize = 10;
 
   @override
   void onInit() {
@@ -19,19 +16,26 @@ abstract class BaseListViewModel<T> extends BaseViewModel {
     getRefreshData();
   }
 
-  /// 下拉刷新
   Future<void> getRefreshData() async {
     _pageNumber = 1;
-    final result = await fetchPage(_pageNumber, _pageSize);
-    datas.assignAll(result);
+    try {
+      final result = await fetchPage(_pageNumber, _pageSize);
+      datas.assignAll(result);
+    } catch (e) {
+      // error handling
+    }
   }
 
-  /// 加载更多
   Future<bool> getLoadData() async {
     _pageNumber++;
-    final result = await fetchPage(_pageNumber, _pageSize);
-    datas.addAll(result);
-    return result.isNotEmpty;
+    try {
+      final result = await fetchPage(_pageNumber, _pageSize);
+      datas.addAll(result);
+      return result.isNotEmpty;
+    } catch (e) {
+      _pageNumber--;
+      return false;
+    }
   }
 
   @override
