eec9a2aa08537bfc9c5f3c8811efad30571bd1f7
x <xuehao0217@gmail.com>
2025-11-17 16:01:18 +0800

    加入android配置

--- common_core/lib/widget/common_widget.dart
+++ common_core/lib/widget/common_widget.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_constraintlayout/flutter_constraintlayout.dart';
 
 class CommonTitleBar extends StatelessWidget {
   final String backIcon;
